ca124e02a38d89251c191543ad60f74fb201f129
DavisAgyemang <davis.agyemang@crowncommercial.gov.uk>
2025-08-26 17:13:50 +0100

    fixed matplotlib error

--- UI_work.py
+++ UI_work.py
@@ -6,6 +6,8 @@ import datetime
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
 import matplotlib.pyplot as plt
+import matplotlib.dates
+matplotlib.use('Agg')
 import matplotlib.dates as mdates
 import io
 app = Flask(__name__)
